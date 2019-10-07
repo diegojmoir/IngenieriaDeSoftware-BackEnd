c4a31053bd2ddf6464a87bd7806a6112343ea53e
Katherine Lorenzo <katherine.lorenzo@vivoconsulting.com>
2019-10-06 20:45:00 -0600

    added users

---
+++ RestauranteAPI/RestauranteAPI/Repositories/Injections/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using RestauranteAPI.Models;
+
+namespace RestauranteAPI.Repositories.Injections
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserAsync(string user, string password);
+    }
+}
