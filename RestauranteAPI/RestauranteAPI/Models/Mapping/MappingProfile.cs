c1f2148e0fa59035f6fc350c029436b5e6a9cbbd
Mynor Xico <xic14659@uvg.edu.gt>
2019-10-09 23:19:14 -0600

    Unit Tests for Registration

--- RestauranteAPI/RestauranteAPI/Models/Mapping/MappingProfile.cs
+++ RestauranteAPI/RestauranteAPI/Models/Mapping/MappingProfile.cs
@@ -14,7 +14,7 @@ namespace RestauranteAPI.Models.Mapping
                 .ForMember(dest=>dest.Key,opt=>opt.MapFrom(src=>src.Key))
                 .ForMember(dest=>dest.FirstName,opt=>opt.MapFrom(src=>src.Object.FirstName))
                 .ForMember(dest=>dest.LastName,opt=>opt.MapFrom(src=>src.Object.LastName))
-                .ForMember(dest=>dest.Email,opt=>opt.MapFrom(src=>src))
+                .ForMember(dest=>dest.Email,opt=>opt.MapFrom(src=>src.Object.Email))
                 .ForMember(dest=>dest.Username,opt=>opt.MapFrom(src=>src.Object.Username))
                 .ForMember(dest=>dest.Password,opt=>opt.MapFrom(src=>src.Object.Password));
         }
